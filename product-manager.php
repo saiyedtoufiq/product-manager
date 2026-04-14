0f3aefa8302311d78f2f08641ef1f6de0cd839e7
saiyed toufiq <saiyedtoufiq@gmail.com>
2026-04-14 14:45:45 +0530

    create plugin

---
+++ product-manager.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: Product Manager
+ * Description: product management plugin for WordPress
+ * Version: 1.0.0
+ * Author: Toufiq rehman sayyed
+ * Text Domain: wp-product-manager
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+
+class Product_Manager {
+    
+}
